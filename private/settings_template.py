e3339a1965855a0c64499424fe11cb7b8d16d8f5
Kathrin Dentler <kathrindentler@Kathrins-MacBook-Air.local>
2018-09-24 12:53:49 +0200

    private settings

---
+++ private/settings_template.py
@@ -0,0 +1,13 @@
+# username and password for your database 
+db_username = "username" 
+db_password = "password"
+
+# to insert a full day, you need to unzip a csv point to its full path, with file name and extension  
+path_to_csv = "/replace/with/full/path/to/csv/filename.csv"
+
+# to bulk insert zipped data, you need to point to its directory 
+path_to_data_folder = "/replace/with/full/path/to/data/directory"
+
+# and also point to a directory where the files are unzipped to; 
+# this is temporarily as they will be deleted afterwards 
+tempbasepath = "/replace/with/full/path/to/temp/directory"
