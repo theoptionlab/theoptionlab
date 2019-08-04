2499d4a9d08a62bab8f2e48ae119e587e367544d
Kathrin Dentler <kathrin.dentler@eccenca.com>
2019-08-05 00:28:32 +0200

    add libor_full.csv path to settings

--- private/settings_template.py
+++ private/settings_template.py
@@ -6,8 +6,11 @@ db_password = "password"
 path_to_csv = "/replace/with/full/path/to/csv/filename.csv"
 
 # to bulk insert zipped data, you need to point to its directory 
-path_to_data_folder = "/replace/with/full/path/to/data/directory"
+path_to_data_directory = "/replace/with/full/path/to/data/directory"
 
 # and also point to a directory where the files are unzipped to; 
 # this is temporarily as they will be deleted afterwards 
 tempbasepath = "/replace/with/full/path/to/temp/directory"
+
+# path to your libor csv 
+path_to_libor_csv = "/replace/with/full/path/to/libor_full.csv"
