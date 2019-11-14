0ea260f6f77b3f6739dc925fb3759e5aa313e692
Kathrin Dentler <kdentler@eccenca.com>
2019-11-14 16:40:51 +0100

    optimize queries to speed up free money scanner

--- optiondata/free_money_scanner.py
+++ optiondata/free_money_scanner.py
@@ -1,76 +1,47 @@
 from __future__ import division 
-import pgdb
+import psycopg2
 from util import util
 from datetime import datetime 
 from private import settings
 
-date = "2019-01-31"
+date = "2019-11-13"
+print (date)
+
 
 counter = 0
 
-db = pgdb.connect(host="localhost", user=settings.db_username, password=settings.db_password, database="optiondata") 
+db = psycopg2.connect(host="localhost", user=settings.db_username, password=settings.db_password, database="optiondata") 
 cur1 = db.cursor()
 cur2 = db.cursor()
 cur3 = db.cursor()
 
-query = "SELECT DISTINCT underlying_symbol FROM fullday WHERE quote_date = '" + str(date) + "' ORDER BY underlying_symbol ASC"
-
+query = "SELECT DISTINCT fullday_call.underlying_symbol, fullday_call.underlying_mid_1545, fullday_call.strike, fullday_call.expiration, (fullday_call.mid_1545 - fullday_put.mid_1545) AS credit FROM fullday as fullday_call JOIN fullday as fullday_put ON fullday_call.underlying_symbol = fullday_put.underlying_symbol AND fullday_call.strike = fullday_put.strike AND fullday_call.expiration = fullday_put.expiration WHERE fullday_call.underlying_mid_1545 > 100 AND fullday_call.expiration > (fullday_call.quote_date + INTERVAL '60 day') AND fullday_call.option_type = 'C' AND fullday_put.option_type = 'P' AND ABS(fullday_call.bid_1545 - fullday_call.ask_1545) < 0.5 AND fullday_call.bid_1545 != 0 AND fullday_call.ask_1545 != 0 AND ABS(fullday_put.bid_1545 - fullday_put.ask_1545) < 0.5 AND fullday_put.bid_1545 != 0 AND fullday_put.ask_1545 != 0 AND (fullday_call.mid_1545 > fullday_put.mid_1545) ORDER BY fullday_call.underlying_symbol, fullday_call.strike, fullday_call.expiration ASC;"
 cur1.execute(query) 
 for row in cur1.fetchall(): 
     underlying = row[0]
-    query = "SELECT distinct underlying_bid_1545, underlying_ask_1545 FROM fullday WHERE underlying_symbol = '" + underlying + "' AND quote_date = '" + str(date) + "'"    
-    cur2.execute(query)
+    underlying_price = row[1]
+    strike = row[2]
+    expiration = row[3]
+    credit = row[4]
 
+    difference = (strike - underlying_price)
+    percentage = float(((difference + credit) / underlying_price) * 100)
+    
+    remaining_time = util.remaining_time(datetime.strptime(str(date), "%Y-%m-%d").date(), datetime.strptime(str(expiration), "%Y-%m-%d"))
+    per_annum = round((percentage / remaining_time), 2)
+    
+    if per_annum > 2.5:
+        counter += 1
+        print("counter: " + str(counter))
+        print("underlying: " + str(underlying)) 
+        print("price: " + str(float(underlying_price)))
+        print("strike: " + str(strike))
+        print("expiration: " + str(expiration))
+        print("credit -c+p: " + str(credit))
+        print("percentage: " + str(round(percentage, 2)))
+        print("remaining time in years: " + str(remaining_time))
+        print("percentage / remaining time: " + str(per_annum))
+        print()
 
-    row = cur2.fetchone()
-    if row == None: 
-        continue 
-    
-    underlying_price = (row[0] + row[1]) / 2
-    if (underlying_price == 0) or (underlying_price < 50): 
-        continue
-    
-    # this query needs this command to actually run: 
-    # SET GLOBAL sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));
-    try: 
-        query = "SELECT DISTINCT strike, expiration, (select ((bid_1545+ask_1545)/2) AS call_mid from fullday where option_type='c' AND id=outer_table.id AND bid_1545 != 0 AND ask_1545 != 0 AND ABS(bid_1545 - ask_1545) < 0.5), (select ((bid_1545+ask_1545)/2) AS put_mid from fullday WHERE bid_1545 != 0 AND ask_1545 != 0 AND ABS(bid_1545 - ask_1545) < 0.5 AND option_type='p' AND strike=outer_table.strike AND expiration=outer_table.expiration AND underlying_symbol = outer_table.underlying_symbol AND quote_date = outer_table.quote_date) FROM fullday as outer_table WHERE bid_1545 != 0 AND ask_1545 != 0 AND underlying_symbol = '"+underlying+"' AND quote_date = '" + date + "' AND expiration > (quote_date + INTERVAL '60 day') GROUP BY id, expiration, strike"
-        cur2.execute(query)
-    except:
-        db.rollback()
-        continue 
-    
-    for row in cur2:        
-        strike = row[0]
-        expiration = row[1]
-        callmidprice = row[2]
-        putmidprice = row[3]        
-        
-        if (callmidprice == None) or (putmidprice == None): 
-            continue 
-        
-        if callmidprice > putmidprice: 
-            
-            credit = callmidprice - putmidprice
-
-            difference = (strike - underlying_price)
-            percentage = float(((difference + credit) / underlying_price) * 100)
-            
-            remaining_time = util.remaining_time(datetime.strptime(str(date), "%Y-%m-%d").date(), datetime.strptime(str(expiration), "%Y-%m-%d"))
-            per_annum = round((percentage / remaining_time), 2)
-            
-            if per_annum > 3:  # USD 12 months LIBOR
-                counter += 1
-                print("counter: " + str(counter))
-                print("underlying: " + str(underlying)) 
-                print("price: " + str(float(underlying_price)))
-                print("strike: " + str(strike))
-                print("expiration: " + str(expiration))
-                print("call midprice: " + str(float(callmidprice)))
-                print("put midprice: " + str(float(putmidprice)))
-                print("credit -c+p: " + str(credit))
-                print("percentage: " + str(round(percentage, 2)))
-                print("remaining time in years: " + str(remaining_time))
-                print("percentage / remaining time: " + str(per_annum))
-                print()
-            
 db.close()
+
--- optiondata/insert_fullday.py
+++ optiondata/insert_fullday.py
@@ -1,25 +1,55 @@
-import pandas
-import numpy as np
-from sqlalchemy import create_engine
+import os  
+import psycopg2
+
 from private import settings
-import pgdb 
+from util import util 
 
-# idea to use unzip function from util
 
-engine = create_engine("postgresql://" + settings.db_username + ":" + settings.db_password + "@127.0.0.1/optiondata")
-table = 'fullday'
+def insert(date):
 
-db = pgdb.connect(host="localhost", user=settings.db_username, password=settings.db_password, database="optiondata") 
-cur2 = db.cursor()
+    datafilepath = settings.path_to_data_directory + "UnderlyingOptionsEODQuotes_" + date + ".zip"
+    unzippedpath = util.unzip(datafilepath)
+    
+    conn = psycopg2.connect("host=localhost dbname=optiondata user=" + settings.db_username)
+    
+    cur = conn.cursor()
+    
+    print("drop table")
+    cur.execute("DROP TABLE fullday")
+    
+    print("create table")
+    cur.execute("CREATE TABLE fullday(underlying_symbol VARCHAR, quote_date date, root VARCHAR, expiration date, strike real, option_type VARCHAR, open real, high real, low real, close real, trade_volume int, bid_size_1545 VARCHAR, bid_1545 real, ask_size_1545 VARCHAR, ask_1545 real, underlying_bid_1545 real, underlying_ask_1545 real, bid_size_eod VARCHAR, bid_eod VARCHAR, ask_size_eod VARCHAR, ask_eod VARCHAR, underlying_bid_eod VARCHAR, underlying_ask_eod VARCHAR, vwap VARCHAR, open_interest VARCHAR, delivery_code VARCHAR)")
+    
+    print("create indices - important!")
+    cur.execute("CREATE INDEX idx_underlying_symbol ON fullday USING btree (underlying_symbol)")
+    cur.execute("CREATE INDEX idx_strike ON fullday USING btree (strike)")
+    cur.execute("CREATE INDEX idx_expiration ON fullday USING btree (expiration)")
+    cur.execute("CREATE INDEX idx_type ON fullday USING btree (option_type)")
+    cur.execute("CREATE INDEX idx_bid_1545 ON fullday USING btree (bid_1545)")
+    cur.execute("CREATE INDEX idx_ask_1545 ON fullday USING btree (ask_1545)")
+    
+    print ("copy data")
+    with open(unzippedpath, 'r') as f:
+        next(f)  # Skip the header row.
+        cur.copy_from(f, 'fullday', sep=',')
+        conn.commit()
+    
+    print("create and update columns underlying_mid_1545 and mid_1545")
+    cur.execute("ALTER TABLE fullday ADD underlying_mid_1545 real")
+    cur.execute("ALTER TABLE fullday ADD mid_1545 real")
+    cur.execute("UPDATE fullday SET underlying_mid_1545 = (underlying_bid_1545 + underlying_ask_1545) / 2")
+    cur.execute("UPDATE fullday SET mid_1545 = (bid_1545 + ask_1545) / 2")
+    
+    conn.commit()
+    
+    cur.execute("SELECT * FROM fullday LIMIT 100")
+    print(cur.fetchall())
+    
+    if unzippedpath != "": 
+        os.remove(unzippedpath)
+    
+    print("done")
 
-reader = pandas.read_csv(settings.path_to_csv, chunksize=10000, header=0, dtype={"underlying_symbol": object, "quote_date": object, "root": object, "expiration": object, "strike": np.float64, "option_type": object, "open": np.float64, "high": np.float64, "low": np.float64, "close": np.float64, "trade_volume": np.int64, "bid_size_1545": np.int64, "bid_1545": np.float64, "ask_size_1545": np.int64, "ask_1545": np.float64, "underlying_bid_1545": np.float64, "underlying_ask_1545": np.float64, "bid_size_eod": np.int64, "bid_eod": np.float64, "ask_size_eod": np.int64, "ask_eod": np.float64, "underlying_bid_eod": np.float64, "underlying_ask_eod": np.float64, "vwap": object, "open_interest": np.float64, "ask_eod": np.float64, "delivery_code": object})                
-for chunk in reader:
-    print(chunk)
-    chunk['option_type'] = chunk.option_type.str.lower()
-    chunk.to_sql(table, engine, if_exists='append', index=False)
 
-db.commit()
-db.close()
-
-print("Done")
-print 
+date = "2019-11-13"
+insert(date)
