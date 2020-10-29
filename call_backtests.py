1c4c75f3b58fb8e32e905dffbb7fcc0f257a5976
Kathrin Dentler <kdentler@eccenca.com>
2020-10-29 16:03:02 +0100

    recompute with fixed data from 2012

--- call_backtests.py
+++ call_backtests.py
@@ -3,7 +3,6 @@ from datetime import datetime
 import time
 
 from strategies import netzero, the_bull, bf70
-
 import backtest_strategies
 
 
@@ -13,8 +12,8 @@ printalot = True
 start = datetime(2006, 11, 1).date()
 end = datetime(2020, 9, 30).date()
 
-# backtest_strategies.backtest(netzero.netzero(), "^RUT", "netzero", 100000, printalot, start, end, netzero.parameters)
-# backtest_strategies.backtest(the_bull.bull(), "^RUT", "the_bull", 100000, printalot, start, end, the_bull.parameters)
+backtest_strategies.backtest(netzero.netzero(), "^RUT", "netzero", 100000, printalot, start, end, netzero.parameters)
+backtest_strategies.backtest(the_bull.bull(), "^RUT", "the_bull", 100000, printalot, start, end, the_bull.parameters)
 backtest_strategies.backtest(bf70.bf70(), "^RUT", "bf70", 100000, printalot, start, end, bf70.parameters)
 
 print()
