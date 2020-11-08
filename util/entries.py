47a85bf429f39ebab04f38e522039d9b2c7acf3d
Kathrin Dentler <kdentler@eccenca.com>
2020-11-08 19:33:07 +0100

    return OrderedDict instead of list with dictionaries

--- util/entries.py
+++ util/entries.py
@@ -4,6 +4,7 @@ from datetime import timedelta, datetime
 from dateutil.relativedelta import relativedelta
 from pandas.tseries.offsets import BMonthEnd
 from util import util 
+import collections 
 
 c = calendar.Calendar(firstweekday=calendar.SUNDAY)
 offset = BMonthEnd()
@@ -51,7 +52,8 @@ def getNextEntry(underlying, refDate, days, regular, eom):
 
 
 def getEntries(underlying, start, end, days, regular, eom):
-    entries = []
+
+    entries = {}
     running = True
     current_date = start 
     
@@ -61,10 +63,10 @@ def getEntries(underlying, start, end, days, regular, eom):
         month = current_date.month
         monthcal = c.monthdatescalendar(year, month)
         third_friday = [day for week in monthcal for day in week if day.weekday() == calendar.FRIDAY and day.month == month][2]
+        
         if regular: 
-                                
-            entry_regular = {}
-            entry_regular['expiration'] = third_friday
+                        
+            expiration = third_friday
             exists = util.connector.check_exists(underlying, third_friday)
 
             if (exists == 0):  # "expiration not found"
@@ -73,50 +75,52 @@ def getEntries(underlying, start, end, days, regular, eom):
                 exists = util.connector.check_exists(underlying, third_saturday)
                 if (exists == 0): 
                     current_date += relativedelta(months=1)
-                    continue;
+                    print ("pass")
+                    pass;
                 
                 else: 
-                    entry_regular['expiration'] = third_saturday 
+                    expiration = third_saturday 
     
-            entry_regular['entrydate'] = entry_regular['expiration'] - timedelta(days) 
-            if (entry_regular['entrydate'] >= start) and (entry_regular['expiration'] <= end): 
-                entries.append(entry_regular)
+            entrydate = expiration - timedelta(days) 
+            if (entrydate >= start) and (expiration <= end): 
+                entries[entrydate] = expiration
                           
-            if entry_regular['entrydate'] >= end: 
+            if entrydate >= end: 
                 running = False  
                 
         if eom:
-            
+                        
             last_day = offset.rollforward(third_friday).date()
             
-            entry_eom = {}
-            entry_eom['expiration'] = last_day
-            entry_eom['entrydate'] = last_day - timedelta(days) 
+            expiration = last_day
+            entrydate = last_day - timedelta(days) 
             
             exists = util.connector.check_exists(underlying, last_day)
             if (exists == 0):  # "expiration not found"
                 day_before = last_day - timedelta(days=1)
                 exists = util.connector.check_exists(underlying, day_before)
                 if (exists == 0): 
-                    continue;
+                    print ("pass")
+                    pass;
                 else: 
-                    entry_eom['expiration'] = day_before
-                    entry_eom['entrydate'] = day_before - timedelta(days) 
+                    expiration = day_before
+                    entrydate = day_before - timedelta(days) 
             
-            if (entry_regular['entrydate'] >= start) and (last_day <= end): 
-                entries.append(entry_eom)
+            if (entrydate >= start) and (last_day <= end): 
+                entries[entrydate] = expiration
                 
-            if entry_regular['entrydate'] >= end: 
+            if entrydate >= end: 
                 running = False  
 
         current_date += relativedelta(months=1)
 
-    sorted_entries = sorted(entries, key=lambda k: k['entrydate']) 
-    return sorted_entries 
+    ordered_entries = collections.OrderedDict(sorted(entries.items(), key=lambda x:x[1], reverse=False))    
+    single_entries = list(ordered_entries.items())
+    return single_entries 
 
 
 def getDailyEntries(underlying, start, end, days):
-    entries = []
+    entries = {}
     running = True
     current_date = start
     
@@ -134,10 +138,9 @@ def getDailyEntries(underlying, start, end, days):
             continue   
         
         expiration = util.connector.select_expiration(current_date, underlying, "p", days)
-        entry = {}
-        entry['entrydate'] = current_date
-        entry['expiration'] = expiration
-        entries.append(entry)
-                
-    return entries 
+        entries[current_date] = expiration
+
+    ordered_entries = collections.OrderedDict(sorted(entries.items(), key=lambda x:x[1], reverse=False))   
+    single_entries = list(ordered_entries.items())
+    return single_entries 
 
