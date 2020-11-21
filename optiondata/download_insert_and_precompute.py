9ed4339e745ba7b7928410ca7693caaaaaf54fe1
Kathrin Dentler <kdentler@eccenca.com>
2020-11-21 19:12:56 +0100

    add reindexing option

--- optiondata/download_insert_and_precompute.py
+++ optiondata/download_insert_and_precompute.py
@@ -2,21 +2,24 @@ from optiondata import insert_data
 from private import settings
 from optiondata import precompute_greeks
 from optiondata import precompute_bs_price
-
-from datetime import timedelta, datetime 
+from util import util 
 from private import download_data
 
 download_data.download(settings.data_dir) 
 
-
 symbols = ["^RUT", "^SPX", "^VIX", "SPLV", "SPHB"]
-
 
 # insert all data from the directory specified in the path. 
 dates = insert_data.insert(symbols, settings.data_dir)
 print (dates)
+print ()
  
 for date in dates: 
     precompute_bs_price.precompute("optiondata", date, "*", True)
     precompute_greeks.precompute("optiondata", date, "*", True)
-
+    
+    
+# if len(dates) > 1: 
+#     print ("reindex")
+#     util.connector.reindex("optiondata")
+#     print ("done")
