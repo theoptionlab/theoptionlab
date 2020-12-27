9e917eef5cee5265754e37a4ccc3ffa1e395717e
Kathrin Dentler <kathrin@dentler.org>
2020-12-27 13:02:02 +0100

    add results up until Christmas 2020

--- call_backtests.py
+++ call_backtests.py
@@ -10,11 +10,11 @@ start_time = time.time()
 printalot = True
 
 start = datetime(2006, 11, 1).date()
-end = datetime(2020, 10, 30).date()
+end = datetime(2021, 1, 1).date()
 
-backtest_strategies.backtest(netzero.netzero(), "^RUT", "netzero", 100000, printalot, start, end, netzero.parameters)
-backtest_strategies.backtest(the_bull.bull(), "^RUT", "the_bull", 100000, printalot, start, end, the_bull.parameters)
 backtest_strategies.backtest(bf70.bf70(), "^RUT", "bf70", 100000, printalot, start, end, bf70.parameters)
+backtest_strategies.backtest(the_bull.bull(), "^RUT", "the_bull", 100000, printalot, start, end, the_bull.parameters)
+backtest_strategies.backtest(netzero.netzero(), "^RUT", "netzero", 100000, printalot, start, end, netzero.parameters)
 
 print()
 print("--- %s seconds ---" % (time.time() - start_time))
