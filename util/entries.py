623222422dbcf52a56ff6f45e7cbd752a942b8df
Kathrin Dentler <kathrin@dentler.org>
2021-03-26 13:43:23 +0100

    refactor entries

--- util/entries.py
+++ util/entries.py
@@ -1,5 +1,5 @@
 import calendar
-from datetime import timedelta 
+from datetime import timedelta
 
 from dateutil.relativedelta import relativedelta
 from pandas.tseries.offsets import BMonthEnd
@@ -10,113 +10,36 @@ c = calendar.Calendar(firstweekday=calendar.SUNDAY)
 offset = BMonthEnd()
 
 
-def getNextEntry(underlying, refDate, days, regular, eom):
-            
-    running = True
-    current_date = refDate
+def is_third_friday_or_saturday(d):
+    return (d.weekday() == 4 and 15 <= d.day <= 21) or (d.weekday() == 5 and 16 <= d.day <= 22)
+
+
+# DTE-based entries. At the moment only monthlies
+# Tried root to get third friday / saturday, but that is not reliable 
+def getEntries(underlying, start, end, days):
     
-    nextEntry = {}
-    
-    while running:
-        
-        year = current_date.year
-        month = current_date.month
-        monthcal = c.monthdatescalendar(year, month)
-        third_friday = [day for week in monthcal for day in week if day.weekday() == calendar.FRIDAY and day.month == month][2]
-        
-        if regular: 
-                        
-            nextExpiration = third_friday
-            exists = util.connector.check_exists(underlying, third_friday)
-
-            if (exists == 0):  # "expiration not found"
-                third_saturday = third_friday + timedelta(days=1)
-                exists = util.connector.check_exists(underlying, third_saturday)
-                if (exists == 0): 
-                    break;
-                
-                else: 
-                    nextExpiration = third_saturday    
-    
-            dte = (nextExpiration - refDate).days
- 
-            if dte >= days: 
-                running = False  
-                
-            else: 
-                nextEntry['expiration'] = nextExpiration
-
-        current_date += relativedelta(months=1)
-
-    return nextEntry 
-
-
-def getEntries(underlying, start, end, days, regular, eom):
-
     entries = {}
-    running = True
-    current_date = start 
-    
-    while running:
-            
-        year = current_date.year
-        month = current_date.month
-        monthcal = c.monthdatescalendar(year, month)
-        third_friday = [day for week in monthcal for day in week if day.weekday() == calendar.FRIDAY and day.month == month][2]
+
+    expirations = util.connector.select_expirations(start + timedelta(days), end + timedelta(days), underlying)
+    for expiration in expirations: 
+        expiration = expiration[0] 
         
-        if regular: 
-                        
-            expiration = third_friday
-            exists = util.connector.check_exists(underlying, third_friday)
-
-            if (exists == 0):  # "expiration not found"
-                third_saturday = third_friday + timedelta(days=1)
-                
-                exists = util.connector.check_exists(underlying, third_saturday)
-                if (exists == 0): 
-                    current_date += relativedelta(months=1)
-                    print ("pass")
-                    pass;
-                
-                else: 
-                    expiration = third_saturday 
-    
-            entrydate = expiration - timedelta(days) 
-            if (entrydate >= start) and (expiration <= end): 
+        if is_third_friday_or_saturday(expiration): 
+            entrydate =  expiration - timedelta(days) 
+        
+            # try for seven days
+            tries = 0
+            while (not util.connector.check_exists(underlying, expiration, entrydate) and tries < 5): 
+                entrydate = entrydate + timedelta(days=1)
+                tries+=1
+            
+            if ((entrydate >= start) and (expiration <= end)): 
                 entries[entrydate] = expiration
-                          
-            if entrydate >= end: 
-                running = False  
-                
-        if eom:
-                        
-            last_day = offset.rollforward(third_friday).date()
-            
-            expiration = last_day
-            entrydate = last_day - timedelta(days) 
-            
-            exists = util.connector.check_exists(underlying, last_day)
-            if (exists == 0):  # "expiration not found"
-                day_before = last_day - timedelta(days=1)
-                exists = util.connector.check_exists(underlying, day_before)
-                if (exists == 0): 
-                    print ("pass")
-                    pass;
-                else: 
-                    expiration = day_before
-                    entrydate = day_before - timedelta(days) 
-            
-            if (entrydate >= start) and (last_day <= end): 
-                entries[entrydate] = expiration
-                
-            if entrydate >= end: 
-                running = False  
-
-        current_date += relativedelta(months=1)
 
     ordered_entries = collections.OrderedDict(sorted(entries.items(), key=lambda x:x[1], reverse=False))    
     single_entries = list(ordered_entries.items())
     return single_entries 
+
 
 
 def getDailyEntries(underlying, start, end, days):
@@ -131,4 +54,46 @@ def getDailyEntries(underlying, start, end, days):
     ordered_entries = collections.OrderedDict(sorted(entries.items(), key=lambda x:x[1], reverse=False))   
     single_entries = list(ordered_entries.items())
     return single_entries 
+
+
+
+# only regular options at the moment 
+def getNextEntry(underlying, refDate, days):
+            
+    running = True
+    current_date = refDate
+    
+    nextEntry = {}
+    
+    while running:
+        
+        year = current_date.year
+        month = current_date.month
+        monthcal = c.monthdatescalendar(year, month)
+        third_friday = [day for week in monthcal for day in week if day.weekday() == calendar.FRIDAY and day.month == month][2]
+        
+                        
+        nextExpiration = third_friday
+        exists = util.connector.check_exists(underlying, third_friday)
+
+        if (exists == 0):  # "expiration not found"
+            third_saturday = third_friday + timedelta(days=1)
+            exists = util.connector.check_exists(underlying, third_saturday)
+            if (exists == 0): 
+                break;
+            
+            else: 
+                nextExpiration = third_saturday    
+
+        dte = (nextExpiration - refDate).days
+
+        if dte >= days: 
+            running = False  
+            
+        else: 
+            nextEntry['expiration'] = nextExpiration
+
+        current_date += relativedelta(months=1)
+
+    return nextEntry 
 
