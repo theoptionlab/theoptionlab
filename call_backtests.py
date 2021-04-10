dade592c402f67722916e929ac8128e3a35f1157
Kathrin Dentler <kathrin@dentler.org>
2021-04-10 10:56:43 +0200

    call stats separately

--- call_backtests.py
+++ call_backtests.py
@@ -4,20 +4,26 @@ import time
 
 from strategies import netzero, the_bull, bf70
 import backtest_strategies
-
+import compute_stats
 
 start_time = time.time()
-printalot = True
-
 start = datetime(2006, 11, 1).date()
 end = datetime(2021, 4, 1).date()
 
 frequency_string = None 
 quantity = None 
+risk_capital = 100000
+include_underlying = True 
+underlying = "^RUT"
 
-backtest_strategies.backtest(bf70.bf70(), "^RUT", "bf70", 100000, quantity, printalot, start, end, bf70.parameters, frequency_string)
-backtest_strategies.backtest(the_bull.bull(), "^RUT", "the_bull", 100000, quantity, printalot, start, end, the_bull.parameters, frequency_string)
-backtest_strategies.backtest(netzero.netzero(), "^RUT", "netzero", 100000, quantity, printalot, start, end, netzero.parameters, frequency_string)
+backtest_strategies.backtest(bf70.bf70(), underlying, "bf70", risk_capital, quantity, start, end, bf70.parameters, frequency_string, include_underlying)
+compute_stats.compute_stats("bf70", underlying, risk_capital)
+
+backtest_strategies.backtest(the_bull.bull(), underlying, "the_bull", risk_capital, quantity, start, end, the_bull.parameters, frequency_string, include_underlying)
+compute_stats.compute_stats("the_bull", underlying, risk_capital)
+#
+backtest_strategies.backtest(netzero.netzero(), underlying, "netzero", risk_capital, quantity, start, end, netzero.parameters, frequency_string, include_underlying)
+compute_stats.compute_stats("netzero", underlying, risk_capital)
 
 print()
 print("--- %s seconds ---" % (time.time() - start_time))
