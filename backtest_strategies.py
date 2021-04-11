a4fb22b3450b409ff416bc1b6c5411978cc31d03
Kathrin Dentler <kathrin@dentler.org>
2021-04-11 18:55:37 +0200

    add continuous option

--- backtest_strategies.py
+++ backtest_strategies.py
@@ -35,12 +35,40 @@ def make_dir(path):
         print ('Created dir %s ' % path)
 
 
-def run_strategies(permutations, strategy_name, parameters, strategy_codes, strategy_path, frequency_string, underlying, start, end, strategy, risk_capital, quantity, include_underlying = False): 
+def get_next_entry (index_nr, frequency_string, single_entries, entry_date, end_date, underlying, dte): 
+    
+
+    if frequency_string == 'C': 
+        entries = {}
+
+        while (entry_date <= end_date):
+
+            expiration = util.connector.select_expiration(entry_date, underlying, "p", dte)
+            if expiration is None: 
+                entry_date = entry_date + timedelta(days=1)
+                continue 
+
+            else: 
+                entries[entry_date] = expiration
+                return list(entries.items())[0]
+
+    else:
+        if index_nr < len(single_entries): 
+            return single_entries[index_nr]
+
+    return None 
+
+
+
+def run_strategies(permutations, strategy_name, parameters, strategy_path, frequency_string, underlying, start, end, strategy, risk_capital, quantity, include_underlying = False): 
     
     trade_log = {}
     i = 0 
 
     for permutation in permutations: 
+        
+        running = True 
+        
         for k, v in permutation.items(): 
             if util.printalot: print(k, v)
 
@@ -50,65 +78,94 @@ def run_strategies(permutations, strategy_name, parameters, strategy_codes, stra
         
         strategy_code = util.derive_strategy_code(permutation, parameters)
         print (strategy_code)
-        strategy_codes.append(strategy_code)
 
         # make dir for permutation 
         strategy_code_path = strategy_path + '/daily_pnls/' + strategy_code + "/"
         make_dir(strategy_code_path)
 
+        single_entries = {}
         # get entries, measure time 
         starttime = time.time()
+        if frequency_string == 'C': 
+            single_entries = {} 
         if frequency_string == 'B': 
             single_entries = entries.getDailyEntries(underlying, start, end, permutation['dte_entry'])
         if frequency_string == 'SMS': 
             single_entries = entries.getSMSEntries(underlying, start, end, permutation['dte_entry'])
-        else: 
+        if frequency_string is None: 
             single_entries = entries.getEntries(underlying, start, end, permutation['dte_entry'])
 
-        print (len(single_entries))
         print('time needed to get entries: ' + format(float(time.time() - starttime), '.2f'))
         
 
         # loop through entries 
-        number_of_trades = 0
-        for e in range(len(single_entries)): 
-
-            entry = single_entries[e]
+        trade_nr = 0
+        index_nr = 0 
+        next_date = None 
         
-            entrydate = entry[0]
-            expiration = entry[1]
+        while (running): 
 
-            if entrydate >= (datetime.now().date() - timedelta(days=7)):
-                break 
+            if next_date is None: next_date = start 
+            entry = get_next_entry(index_nr, frequency_string, single_entries, next_date, end, underlying, permutation['dte_entry']) 
             
-            if strategy_name == 'the_bull': 
-                permutation['dte_exit'] = 37
-                try: 
-                    next_entry = single_entries[e + 1]
-                    next_entrydate = next_entry[0]
-                    permutation['dte_exit'] = 66 - (next_entrydate - entrydate).days
-                except IndexError: 
-                    continue
+            if entry is not None: 
+                entrydate = entry[0]
+                expiration = entry[1]
+    
+                if entrydate >= (datetime.now().date() - timedelta(days=7)):
+                    break 
                 
-            # run with parameters 
-            strategy.setParameters(permutation['patient_days_before'], permutation['patient_days_after'], permutation['cheap_entry'], permutation['down_day_entry'], permutation['patient_entry'], 
-                                   permutation['min_vix_entry'], permutation['max_vix_entry'], 
-                                   permutation['min_iv_entry'], permutation['max_iv_entry'], 
-                                   permutation['sma_window'], 
-                                   permutation['dte_entry'], permutation['els_entry'], permutation['ew_exit'], permutation['pct_exit'], permutation['dte_exit'], permutation['dit_exit'], permutation['deltatheta_exit'], permutation['tp_exit'], permutation['sl_exit'], permutation['delta'])
-            result = run_strategy.fly(strategy, underlying, risk_capital, quantity, entrydate, expiration)
+                if strategy_name == 'the_bull': 
+                    permutation['dte_exit'] = 37
+                    try: 
+                        next_entry = get_next_entry(index_nr + 1, frequency_string, single_entries, next_date, end, underlying, permutation['dte_entry']) 
+                        if next_entry is not None: 
+                            permutation['dte_exit'] = 66 - (next_entry[0] - entrydate).days
+                    except Exception as e:
+                        print (e)
+                    
+                # run with parameters 
+                strategy.setParameters(permutation['patient_days_before'], 
+                permutation['patient_days_after'], 
+                permutation['cheap_entry'], 
+                permutation['down_day_entry'], 
+                permutation['patient_entry'], 
+                permutation['min_vix_entry'], 
+                permutation['max_vix_entry'], 
+                permutation['min_iv_entry'], 
+                permutation['max_iv_entry'], 
+                permutation['sma_window'], 
+                permutation['dte_entry'], 
+                permutation['els_entry'], 
+                permutation['ew_exit'], 
+                permutation['pct_exit'], 
+                permutation['dte_exit'], 
+                permutation['dit_exit'], 
+                permutation['deltatheta_exit'], 
+                permutation['tp_exit'], 
+                permutation['sl_exit'], 
+                permutation['delta'])
+                result = run_strategy.fly(strategy, underlying, risk_capital, quantity, entrydate, expiration)
 
-            if (not result is None): 
-                number_of_trades += 1
-                i += 1
+                if (not result is None): 
+                    trade_nr += 1
+                    i += 1
+    
+                    # save dailypnls 
+                    file_name = strategy_code_path + str(i) + '.csv'
+                    result['dailypnls'].to_csv(file_name)
+                    del result['dailypnls']
+    
+                    trade_log[i] = dict({'trade nr.' : trade_nr, 'strategy_code': strategy_code}, **result)
+                    print (trade_log[i])
+                    next_date = (result["exit_date"]) + timedelta(days=1)
 
-                # save dailypnls 
-                file_name = strategy_code_path + str(i) + '.csv'
-                result['dailypnls'].to_csv(file_name)
-                del result['dailypnls']
+                else: next_date = next_date + timedelta(days=1)
+                    
+                index_nr += 1 
+            
+            else: running = False 
 
-                trade_log[i] = dict({'trade nr.' : number_of_trades, 'strategy_code': strategy_code}, **result)
-                print (trade_log[i])
 
     if (include_underlying): 
         # create results file for underlying   
@@ -156,10 +213,9 @@ def run_strategies(permutations, strategy_name, parameters, strategy_codes, stra
         make_dir(underlying_strategy_code_path)
         underlying_file_name = underlying_strategy_code_path + 'underlying.csv'
         underlying_daily_pnls.to_csv(underlying_file_name)
-        strategy_codes.append(str(underlying.replace("^","")))
         
         # save underlying to trade_log
-        trade_log[i+1] = dict({'trade nr.' : number_of_trades+1, 'strategy_code': str(underlying.replace("^","")), 'entry_date': underlying_start_date, 'expiration': None, 'exit_date': underlying_date, 'entry_underlying': str(format(float(underlying_at_entry), '.2f')), 'entry_vix': entry_vix, 'strikes': None, 'iv_legs': None, 'entry_price': str(format(float(risk_capital), '.2f')), 'dte' : 0, 'dit' : (end - start).days, 'pnl': str(format(float(current_pnl), '.2f')), 'dailypnls' : None, 'max_risk' : str(format(float(risk_capital), '.2f')), 'position_size' : underlying_multiplier, 'percentage': str(format(float(round((float(current_pnl) / risk_capital) * 100, 2)), '.2f')) + '%', 'exit': None})
+        trade_log[i+1] = dict({'trade nr.' : trade_nr+1, 'strategy_code': str(underlying.replace("^","")), 'entry_date': underlying_start_date, 'expiration': None, 'exit_date': underlying_date, 'entry_underlying': str(format(float(underlying_at_entry), '.2f')), 'entry_vix': entry_vix, 'strikes': None, 'iv_legs': None, 'entry_price': str(format(float(risk_capital), '.2f')), 'dte' : 0, 'dit' : (end - start).days, 'pnl': str(format(float(current_pnl), '.2f')), 'dailypnls' : None, 'max_risk' : str(format(float(risk_capital), '.2f')), 'position_size' : underlying_multiplier, 'percentage': str(format(float(round((float(current_pnl) / risk_capital) * 100, 2)), '.2f')) + '%', 'exit': None})
 
     # finished looping, save trade_log 
     df_log = pd.DataFrame.from_dict(trade_log, orient='index')
@@ -178,9 +234,7 @@ def backtest(strategy, underlying, strategy_name, risk_capital, quantity, start,
     # prepare 
     if util.printalot: print('number of combinations: ' + str(len(list(dict_product(parameters)))))
     permutations = dict_product(parameters)
-    
-    strategy_codes = []
-    
+        
     # create directories  
     path = os.getcwd()
     make_dir(path + '/results')
@@ -193,5 +247,5 @@ def backtest(strategy, underlying, strategy_name, risk_capital, quantity, start,
         print(e)
     make_dir(strategy_path + '/daily_pnls')
     
-    run_strategies(permutations, strategy_name, parameters, strategy_codes, strategy_path, frequency_string, underlying, start, end, strategy, risk_capital, quantity, include_underlying)
+    run_strategies(permutations, strategy_name, parameters, strategy_path, frequency_string, underlying, start, end, strategy, risk_capital, quantity, include_underlying)
 
