f03f761a7df8a18f3ba87e4bcdadc179a682df88
Kathrin Dentler <kathrin.dentler@triply.cc>
2021-12-30 23:55:30 +0100

    update fullday and scanner

--- free_money_scanner.py
+++ free_money_scanner.py
@@ -1,23 +1,22 @@
-from __future__ import division 
+from __future__ import division
 import psycopg2
 from util import util
-from datetime import datetime 
+from datetime import datetime
 from private import settings
 
-date = "2021-10-19"
-print (date)
-print ()
+date = "2010-08-03"
+print(date)
+print()
 
 
 counter = 0
 
-db = psycopg2.connect(host="localhost", user=settings.db_username, password=settings.db_password, database="optiondata") 
+db = psycopg2.connect(host="localhost", user=settings.db_username,
+                      password=settings.db_password, database="optiondata")
 cur1 = db.cursor()
-print ("before query")
 query = "SELECT DISTINCT fullday_call.underlying_symbol, fullday_call.underlying_mid_1545, fullday_call.strike, fullday_call.expiration, (fullday_call.mid_1545 - fullday_put.mid_1545) AS credit FROM fullday as fullday_call JOIN fullday as fullday_put ON fullday_call.underlying_symbol = fullday_put.underlying_symbol AND fullday_call.strike = fullday_put.strike AND fullday_call.expiration = fullday_put.expiration WHERE fullday_call.underlying_mid_1545 > 100 AND fullday_call.expiration > (fullday_call.quote_date + INTERVAL '60 day') AND fullday_call.option_type = 'c' AND fullday_put.option_type = 'p' AND ABS(fullday_call.bid_1545 - fullday_call.ask_1545) < 0.5 AND fullday_call.bid_1545 != 0 AND fullday_call.ask_1545 != 0 AND ABS(fullday_put.bid_1545 - fullday_put.ask_1545) < 0.5 AND fullday_put.bid_1545 != 0 AND fullday_put.ask_1545 != 0 AND (fullday_call.mid_1545 > fullday_put.mid_1545) ORDER BY fullday_call.underlying_symbol, fullday_call.strike, fullday_call.expiration ASC;"
-cur1.execute(query) 
-print ("after query")
-for row in cur1.fetchall(): 
+cur1.execute(query)
+for row in cur1.fetchall():
     underlying = row[0]
     underlying_price = row[1]
     strike = row[2]
@@ -26,14 +25,15 @@ for row in cur1.fetchall():
 
     difference = (strike - underlying_price)
     percentage = float(((difference + credit) / underlying_price) * 100)
-    
-    remaining_time = util.remaining_time(datetime.strptime(str(date), "%Y-%m-%d").date(), datetime.strptime(str(expiration), "%Y-%m-%d"))
+
+    remaining_time = util.remaining_time(datetime.strptime(
+        str(date), "%Y-%m-%d").date(), datetime.strptime(str(expiration), "%Y-%m-%d"))
     per_annum = round((percentage / remaining_time), 2)
-    
-    if per_annum > 1:
+
+    if per_annum > 5:
         counter += 1
         print("counter: " + str(counter))
-        print("underlying: " + str(underlying)) 
+        print("underlying: " + str(underlying))
         print("price: " + str(float(underlying_price)))
         print("strike: " + str(strike))
         print("expiration: " + str(expiration))
@@ -44,4 +44,3 @@ for row in cur1.fetchall():
         print()
 
 db.close()
-
